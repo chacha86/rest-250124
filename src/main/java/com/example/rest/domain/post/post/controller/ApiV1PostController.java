b80b2cbcdd350a3251bd227b9bdd2c6996f223d0
chacha86 <taejincha0619@gmail.com>
2025-01-24 15:56:04 +0900

    rsData

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -5,7 +5,9 @@ import com.example.rest.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -20,11 +22,16 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable long id) {
+    public Map<String, Object> delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        return "%d번 글 삭제가 완료되었습니다.".formatted(id);
+
+        Map<String, Object> rsData = new HashMap<>();
+        rsData.put("code", "200-1");
+        rsData.put("msg", "%d번 글 삭제가 완료되었습니다.".formatted(id));
+
+        return rsData;
     }
 
 }
