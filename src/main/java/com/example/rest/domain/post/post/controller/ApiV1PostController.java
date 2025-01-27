ff04baa382d76c73373fcfc2dea7cf91cc8c7d18
chacha86 <taejincha0619@gmail.com>
2025-01-27 12:36:02 +0900

    generic apply

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,14 +21,14 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
-    public RsData getItems() {
+    public RsData<List<PostDto>> getItems() {
 
         List<Post> posts = postService.getItems();
         List<PostDto> postDtos = posts.stream()
                 .map(PostDto::new)
                 .toList();
 
-        return new RsData(
+        return new RsData<>(
                 "200-1",
                 "글 목록 조회가 완료되었습니다.",
                 postDtos
@@ -36,13 +37,11 @@ public class ApiV1PostController {
 
 
     @GetMapping("{id}")
-    public RsData getItem(@PathVariable long id) {
+    public RsData<PostDto> getItem(@PathVariable long id) {
 
         Post post = postService.getItem(id).get();
-//        PostDto postDto = new PostDto(post);
-//        return postDto;
 
-        return new RsData(
+        return new RsData<> (
                 "200-1",
                 "글 조회가 완료되었습니다.",
                 new PostDto(post)
@@ -84,7 +83,7 @@ public class ApiV1PostController {
     public RsData write(@RequestBody @Valid WriteReqBody body) {
         Post post = postService.write(body.title(), body.content());
 
-        return new RsData(
+        return new RsData<Long>(
                 "200-1",
                 "글 작성이 완료되었습니다.",
                 post.getId()
