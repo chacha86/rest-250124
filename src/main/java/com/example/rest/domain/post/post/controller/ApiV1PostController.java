fe8602f9fedba24eb3f01e7e6777bc3727ef7ec3
chacha86 <taejincha0619@gmail.com>
2025-01-27 11:34:56 +0900

    rsData rs field add

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -38,7 +38,7 @@ public class ApiV1PostController {
 
         return postDto;
     }
-    
+
     @DeleteMapping("/{id}")
     public RsData delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
@@ -72,11 +72,12 @@ public class ApiV1PostController {
 
     @PostMapping
     public RsData write(@RequestBody @Valid WriteReqBody body) {
-        postService.write(body.title(), body.content());
+        Post post = postService.write(body.title(), body.content());
 
         return new RsData(
                 "200-1",
-                "글 작성이 완료되었습니다."
+                "글 작성이 완료되었습니다.",
+                post.getId()
         );
     }
 
--- src/main/java/com/example/rest/global/dto/RsData.java
+++ src/main/java/com/example/rest/global/dto/RsData.java
@@ -8,4 +8,10 @@ import lombok.Getter;
 public class RsData {
     private String code;
     private String msg;
+    private long id;
+
+    public RsData(String code, String msg) {
+        this(code, msg, 0);
+    }
+
 }
